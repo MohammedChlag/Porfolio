3da29472459a9c3de5836dff54d2ab1a25a1d630
Mohammed Chikri <mohammedchikrilag@icloud.com>
2025-04-03 00:37:26 +0200

    Mi porfolio esta cogiendo forma

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').config}*/
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
